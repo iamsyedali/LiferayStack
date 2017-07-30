dd34001b81fb080ac5a5c115ba46d7b1bde4df8b
Syed Ali <iamsyedali@users.noreply.github.com>
2017-07-30 09:36:48 +0530

    commited recent files

--- LiferayWorkspace/maven-modules/products-data/products-data-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,3 +0,0 @@
-create index IX_A9A28F13 on LS_Product (groupId, status);
-create index IX_287E0CD1 on LS_Product (uuid_[$COLUMN_LENGTH:75$], companyId);
-create unique index IX_51159393 on LS_Product (uuid_[$COLUMN_LENGTH:75$], groupId);
--- LiferayWorkspace/maven-modules/products-portlet/src/main/java/com/liferaystack/products/constants/ProductPortletKeys.java
+++
@@ -1,5 +0,0 @@
-package com.liferaystack.products.constants;
-
-public interface ProductPortletKeys {
-	final String PRODUCT_PORTLET_NAME = "com_liferaystack_products_portlet_ProductsPortlet";
-}
