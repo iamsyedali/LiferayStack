1b14ec4fe5e367d8f18e6de9cccb60eb669310bf
Syed Ali <iamsyedali@users.noreply.github.com>
2017-08-26 10:16:40 +0530

    test-web portlet commited

---
+++ LiferayWorkspace/modules/test-web/src/main/java/test/web/constants/TestWebPortletKeys.java
@@ -0,0 +1,10 @@
+package test.web.constants;
+
+/**
+ * @author Syed Ali
+ */
+public class TestWebPortletKeys {
+
+	public static final String TestWeb = "TestWeb";
+
+}
---
+++ LiferayWorkspace/modules/test-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="test-web.caption"/></b>
+</p>
