84365c955b3cdbba95d8314067ed02938b4acd3f
Syed Ali <iamsyedali@users.noreply.github.com>
2017-09-14 18:41:23 +0530

    example portlet added

---
+++ LiferayWorkspace/modules/example-web/src/main/java/com/liferaystack/example/constants/ExampleWebPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferaystack.example.constants;
+
+/**
+ * @author Syed Ali
+ */
+public class ExampleWebPortletKeys {
+
+	public static final String ExampleWeb = "ExampleWeb";
+
+}
---
+++ LiferayWorkspace/modules/example-web/src/main/java/com/liferaystack/example/portlet/ExampleWebPortlet.java
@@ -0,0 +1,29 @@
+package com.liferaystack.example.portlet;
+
+import com.liferaystack.example.constants.ExampleWebPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Syed Ali
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=example-web Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ExampleWebPortletKeys.ExampleWeb,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ExampleWebPortlet extends MVCPortlet {
+}
